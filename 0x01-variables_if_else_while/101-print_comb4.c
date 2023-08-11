28992a3deafc279795f2cfeca3616b9ba9bc66e1
promisechika <promiseonwutuebe@gmail.com>
2023-08-11 14:58:33 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - main function
+ * main - print the last digit of the number stored in the variable n
  *
  * Return: always 0
  */
@@ -14,9 +14,9 @@ int main(void)
 
 	for (i = 0 ; i < 8 ; i++)
 	{
-		for (j = i + 1 ; i < 9 ; j++)
+		for (j = i + 1 ; j < 9 ; j++)
 		{
-			for (k = j + 1 ; j < 10 ; k++)
+			for (k = j + 1 ; k < 10 ; k++)
 			{
 				putchar((i % 10) + '0');
 				putchar((j % 10) + '0');
