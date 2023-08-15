413e4346ddc9ed89d280d3b0ef2f4faa3fd3a94f
promisechika <promiseonwutuebe@gmail.com>
2023-08-15 15:55:27 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - add two integers and return the value
+ * @i: the first integer
+ * @j: the second integer
+ * Return: integer value
+ */
+
+int add(int i, int j)
+{
+	return (i + j);
+}
