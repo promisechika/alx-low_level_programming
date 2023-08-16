d77a2888021e6db57c58f536d2ae1f8539be6822
promisechika <promiseonwutuebe@gmail.com>
2023-08-16 16:29:11 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,32 +7,21 @@
 
 int main(void)
 {
-	unsigned long int a, b, c, b1, b2, c1, c2;
+	unsigned int i;
+	unsigned long int a, b, c;
 
-	b = 1;
-	c = 2;
+	a = 1;
+	b = 2;
 
-	printf("%lu", b);
+	printf("%lu, %lu", a, b);
 
-	for (a = 1 ; a <= 91 ; a++)
+	for (i = 2 ; i < 98 ; ++i)
 	{
+		c = a + b;
 		printf(", %lu", c);
-		c = c + b;
-		b = c - b;
-	}
-	b1 = b / 1000000000;
-	b2 = b % 1000000000;
-	c1 = c / 1000000000;
-	c2 = c % 1000000000;
 
-	for (a = 92 ; a < 99 ; ++a)
-	{
-		printf(", %lu", c1 + (c2 / 1000000000));
-		printf("%lu", c2 % 1000000000);
-		c1 = c1 + b1;
-		b1 = c1 - b1;
-		c2 = c2 + b2;
-		b2 = c2 - b2;
+		a = b;
+		b = c;
 	}
 	printf("\n");
 	return (0);
