f245278cbbcdda876a137372929a3a190e1b658b
promisechika <promiseonwutuebe@gmail.com>
2023-08-28 04:50:22 -0700

    5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strstr - function that locates a substring
+ * @haystack: pointer to search string
+ * @needle: pointer to locate substring
+ * Return: haystack or NULL
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	while (*haystack != '\0')
+	{
+		char *h = haystack;
+		char *n = needle;
+
+		while (*n != '\0' && *h == *n)
+		{
+			h++;
+			n++;
+		}
+		if (*n == '\0')
+			return (haystack);
+		haystack++;
+	}
+	return (NULL);
+}
