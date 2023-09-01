00414f5b7bb2874249e256c26b09e09873df9ef3
promisechika <promiseonwutuebe@gmail.com>
2023-09-01 07:29:59 -0700

    mynewnameis

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints a name
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
