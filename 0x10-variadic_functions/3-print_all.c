50d957d84b7d3e344bc94d1056e0d06ed4ab12e9
promisechika <promiseonwutuebe@gmail.com>
2023-09-14 07:28:43 -0700

    d

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,48 @@
+#include "variadic_functions.h"
+
+/**
+ * print_all -  function that prints anything
+ * @format: string containing the types of argument
+ * @...: string to be printed
+ */
+
+void print_all(const char * const format, ...)
+{
+	va_list args;
+	char *separator = "";
+	unsigned int i = 0;
+
+	va_start(args, format);
+
+	while (format && format[i])
+	{
+		switch (format[i])
+		{
+			case 'c':
+				printf("%s%c", separator, va_arg(args, int));
+				break;
+			case 'i':
+				printf("%s%d", separator, va_arg(args, int));
+				break;
+			case 'f':
+				printf("%s%f", separator, va_arg(args, double));
+				break;
+			case 's':
+			{
+				char *str = va_arg(args, char *);
+
+				if (str == NULL)
+					str = "(nil)";
+				printf("%s%s", separator, str);
+				break;
+			}
+			default:
+			i++;
+			continue;
+		}
+		separator = ", ";
+		i++;
+	}
+	va_end(args);
+	printf("\n");
+}
