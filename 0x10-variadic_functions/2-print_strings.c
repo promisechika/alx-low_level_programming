e318c676795ca00b98acd8d6e66da2f3aec8dca9
promisechika <promiseonwutuebe@gmail.com>
2023-09-14 06:50:53 -0700

    c

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,36 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - function that prints strings
+ * @separator: the string to be printed
+ * @n: number of srings
+ * @...: the strings to be printed
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+
+	va_start(args, n);
+
+	for (i = 0 ; i < n ; i++)
+	{
+		char *str = va_arg(args, char *);
+
+		if (str != NULL)
+		{
+			printf("%s", str);
+		}
+		else
+		{
+			printf("(nil)");
+		}
+		if (separator != NULL && i < n - 1)
+		{
+			printf("%s", separator);
+		}
+	}
+	va_end(args);
+	printf("\n");
+}
