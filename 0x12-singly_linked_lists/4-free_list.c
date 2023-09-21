74c2830f941e0bfbc1b8b702b24f4dcd09720b76
promisechika <promiseonwutuebe@gmail.com>
2023-09-21 07:24:12 -0700

    task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_list - Frees a list_t linked list.
+ * @head: A pointer to the head of the list.
+ */
+
+void free_list(list_t *head)
+{
+	list_t *current;
+	list_t *next;
+
+	current = head;
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current->str);
+		free(current);
+		current = next;
+	}
+}
