524ca92c0fe753457c0d38f8bf8fc9c281f64eb4
promisechika <promiseonwutuebe@gmail.com>
2023-09-26 16:12:42 -0700

    8-sum_listint.c

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * sum_listint -  function that returns the sum of all the data
+ * @head: pointer
+ * Return: sum
+ */
+
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+	listint_t *temp = head;
+
+	while (temp)
+	{
+		sum += temp->n;
+		temp = temp->next;
+	}
+	return (sum);
+}
