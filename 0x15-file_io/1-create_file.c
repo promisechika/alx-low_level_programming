700892fd33e695d1175e7cf8ff5771b68e01dc93
promisechika <promiseonwutuebe@gmail.com>
2023-10-02 08:06:19 -0700

    b

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * create_file - function that creates a file
+ * @filename: name of file to create
+ * @text_content: pointer
+ * Return: 1 or -1
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int f, g, h = 0;
+
+	if (filename == NULL)
+		return (-1);
+	if (text_content != NULL)
+	{
+		for (h = 0; text_content[h];)
+			h++;
+	}
+	f = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	g = write(f, text_content, h);
+	if (f == -1 || g == -1)
+		return (-1);
+	close(f);
+	return (1);
+}
